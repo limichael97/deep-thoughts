1ee11fdfa900ddad68df0755866c480763a028dc
Michael Li <thisislimichael@gmail.com>
2022-02-22 23:46:40 -0800

    added mutations file

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+
+  const [addUser, { error }] = useMutation(ADD_USER);
+
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -14,9 +19,20 @@ const Signup = () => {
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
+  // submit form 
+  const handleFormSubmit = async event => {
     event.preventDefault();
+
+    //  try/catch instead of promises to handle errors
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   
